c463f5606fada05f4aeed606a257d2063cb5ab83
yyy <1114884663@qq.com>
2023-05-16 10:18:40 +0800

    modify the data to deploy

--- testenvmanage-masternode-server/src/main/java/cn/com/devopsplus/dop/server/testenvmanage/mq/MessageConsumer.java
+++ testenvmanage-masternode-server/src/main/java/cn/com/devopsplus/dop/server/testenvmanage/mq/MessageConsumer.java
@@ -73,10 +73,10 @@ public class MessageConsumer implements MessageListenerOrderly {
      */
     @Override
     public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
-        while(this.dataTransmitService.getFreeNodeNumber()==0){}
         int index = 0;
         try {
             for (; index < msgs.size(); index++) {
+                while(this.dataTransmitService.getFreeNodeNumber()==0){}
                 MessageExt msg = msgs.get(index);
                 String messageBody = new String(msg.getBody(), RemotingHelper.DEFAULT_CHARSET);
                 logger.info("MQ：MessageConsumer接收新信息: msgId={} topic={} tags={} keys={} messageBody={}", msg.getMsgId(), msg.getTopic(), msg.getTags(), msg.getKeys(), messageBody);
