9be7a7abb388b91067d461aaba0851366337ec24
yyy <1114884663@qq.com>
2023-05-18 16:20:09 +0800

    modify to integrate and deploy to ArtDev Platform

--- masteragent/src/main/java/com/example/agent/util/RedisUtil.java
+++ masteragent/src/main/java/com/example/agent/util/RedisUtil.java
@@ -1,6 +1,7 @@
 package com.example.agent.util;
 
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,7 @@ import java.util.List;
 @Component
 public class RedisUtil{
 
-    @Resource
+    @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
     /**
