ec761663e9e0822add2e990e5dbde1abff8a76f8
yyy <1114884663@qq.com>
2023-05-22 01:02:31 +0800

    modify to integrate and deploy to ArtDev Platform

--- slaveagent/src/main/java/com/example/agent/AgentApplication.java
+++ slaveagent/src/main/java/com/example/agent/AgentApplication.java
@@ -3,11 +3,13 @@ package com.example.agent;
 import com.example.agent.util.BeanUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class AgentApplication {
 
 	public static void main(String[] args) {
