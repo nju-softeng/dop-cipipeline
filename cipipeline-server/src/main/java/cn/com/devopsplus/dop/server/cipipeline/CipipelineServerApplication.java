f042fa3dadaa608d191f79aa97a56e8200d55abd
yyy <1114884663@qq.com>
2023-05-31 15:38:57 +0800

    modify the data to deploy

--- cipipeline-server/src/main/java/cn/com/devopsplus/dop/server/cipipeline/CipipelineServerApplication.java
+++ cipipeline-server/src/main/java/cn/com/devopsplus/dop/server/cipipeline/CipipelineServerApplication.java
@@ -3,11 +3,13 @@ package cn.com.devopsplus.dop.server.cipipeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableEurekaClient
+@EnableFeignClients
 public class CipipelineServerApplication {
 
     public static void main(String[] args) {
